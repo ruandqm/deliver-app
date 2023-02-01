30f96d2fc54506559ce6c04c1da881c00c90efc4
ruandqm <ruandqm@gmail.com>
2023-02-01 20:21:55 -0300

    container classes added in all components

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,16 +4,12 @@ import "./style.scss"
 const Img: React.FC<IRestaurant> = (props) => {
 
     return (
-        
-            <div className="card">
-
-                <img src={props.url} />
-                <h3>{props.nome}</h3>
-                <p>{props.avaliacao}</p> <span>{props.categoria}</span>
-                <p>{props.sobre}</p>
-
-            </div>
-
+        <div className="cardContainer">
+            <img src={props.url} />
+            <h3>{props.nome}</h3>
+            <p>{props.avaliacao}</p> <span>{props.categoria}</span>
+            <p>{props.sobre}</p>
+        </div>
     )
 }
 
