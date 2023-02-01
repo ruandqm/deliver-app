1a66dd19e7fcde45dcb6d69748beda3b753f2140
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-02-01 19:43:54 -0300

    creating components for api reading

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Navbar } from '../../components/Navbar/index'
+import SearchRestaurant from '../../components/SearchRestaurant/SearchRestaurant'
 
 export const Home = () => {
+
     return (
-        <div className='homeContainer'>
-            <Navbar isHome={true} />
-        </div>
+        <>
+
+            <div className='homeContainer'>
+                <Navbar isHome={true} />
+            </div>
+
+                <SearchRestaurant />
+
+        </>
+
     )
 }
