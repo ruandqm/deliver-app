f8c735f26441763476e35565b1c0ff926df0cf93
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-02-05 22:34:53 -0300

    fixing conflict

--- src/contexts/contexts.tsx
+++ src/contexts/contexts.tsx
@@ -1,6 +1,7 @@
 import React, { createContext } from "react";
 
 export const HomeContext = createContext(null as React.ComponentState)
+
 export const RestaurantRuanContext = createContext(null as React.ComponentState)
 
 export const RestaurantMarcosContext = createContext(null as React.ComponentState)
